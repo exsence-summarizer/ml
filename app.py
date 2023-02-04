a63f6640e51e235f8e8d41d7f027bd9afa2798de
Anas Ansari <77924312+anas-ansri@users.noreply.github.com>
2023-02-05 00:18:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 # sample home page

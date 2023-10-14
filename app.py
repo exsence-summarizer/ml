8c8749b991c60c16bb137b18ab50bdaf33ff6ff7
Anas Ansari <77924312+anas-ansri@users.noreply.github.com>
2023-10-14 15:45:20 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from type_1 import summarize_text
 # load api
 app = FastAPI()
 
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # sample home page
